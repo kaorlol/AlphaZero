39ff79413e9365a55d4ab88a4ab22a41065839d7
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-03-16 18:18:41 -0400

    Add files via upload

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/AlphaZero/main/Loader.lua")))();
+if isfile("AlphaZero/Loader.lua") then
+    loadfile("AlphaZero/Loader.lua")();
+else
+    local Status, Script = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Uvxtq/AlphaZero/main/Loader.lua");
+
+    if Status then
+        loadstring(Script)()
+    else
+        game:GetService("Players").LocalPlayer:Kick("Failed to grab loader, join the discord for support.");
+    end
+end
