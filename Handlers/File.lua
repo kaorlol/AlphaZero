2bc32b8ecc8a78091b8b20e6743ad6d95e7691ea
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-04-03 07:57:10 -0400

    Add files via upload

--- Handlers/File.lua
+++ Handlers/File.lua
@@ -1,9 +1,12 @@
+local function GetLastCommit()
+    local Response = game:HttpGetAsync("https://api.github.com/repos/Uvxtq/AlphaZero/branches/main");
+    local Data = game:GetService("HttpService"):JSONDecode(Response);
+
+    return Data.commit.sha;
+end
+
 local FileHandler = {}; do
     function FileHandler:Setup(Hub: string, Version: string, Subfolders: table)
-        assert(typeof(Hub) == "string", "Hub must be a string");
-        assert(typeof(Subfolders) == "table", "Subfolders must be a table");
-        assert(typeof(Version) == "string", "Version must be a string");
-
         warn("Setting up file handler: "..Hub);
 
         if not isfolder(Hub) then
@@ -14,6 +17,7 @@ local FileHandler = {}; do
             end
 
             self:Write(Hub.."/Version.txt", Version);
+            self:Write(Hub.."/LastCommit.txt", GetLastCommit());
 
             warn("Completed setup of file handler: "..Hub);
 
@@ -48,18 +52,10 @@ local FileHandler = {}; do
     end;
 
     function FileHandler:Write(Path: string, Content: any)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(typeof(Content) == "string" or typeof(Content) == "number", "Content must be a string or number");
-        assert(not isfolder(Path), "Cannot write to a folder");
-
         writefile(Path, Content);
     end;
 
     function FileHandler:Append(Path: string, Content: any)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(typeof(Content) == "string" or typeof(Content) == "number", "Content must be a string or number");
-        assert(not isfolder(Path), "Cannot write to a folder");
-
         if not isfile(Path) then
             return self:Write(Path, Content);
         end
@@ -68,27 +64,16 @@ local FileHandler = {}; do
     end;
 
     function FileHandler:Read(Path: string)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(not isfolder(Path), "Cannot read a folder");
-        assert(isfile(Path), "File does not exist");
-
         return readfile(Path);
     end;
 
     function FileHandler:Load(Path: string)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(not isfolder(Path), "Cannot load a folder");
-        assert(isfile(Path), "File does not exist");
-
         warn("Loaded file: "..Path);
 
         return loadfile(Path)();
     end;
 
     function FileHandler:Delete(Path: string)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(isfile(Path) or isfolder(Path), "File or folder does not exist");
-
         if isfile(Path) then
             delfile(Path);
         else
@@ -97,19 +82,15 @@ local FileHandler = {}; do
     end;
 
     function FileHandler:Exists(Path: string)
-        assert(typeof(Path) == "string", "Path must be a string");
-
         return isfile(Path) or isfolder(Path);
     end;
 
     function FileHandler:Download(Path: string, Url: string)
-        assert(typeof(Path) == "string", "Path must be a string");
-        assert(typeof(Url) == "string", "Url must be a string");
-
         if self:Exists(Path) then
             local Content = self:Read(Path)
+            local LastCommit = self:Read("AlphaZero/LastCommit.txt");
 
-            if #Content == #game:HttpGet(Url) then
+            if LastCommit ~= GetLastCommit() then
                 warn("No changes have been made to " .. Path)
 
                 return;
@@ -122,8 +103,6 @@ local FileHandler = {}; do
     end;
 
     function FileHandler:GetFilesFrom(Folder: string)
-        assert(typeof(Folder) == "string", "Folder must be a string");
-
         local Response = game:HttpGetAsync(Folder);
         local Files = {};
 
