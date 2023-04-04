9bdd8ee3a4e0333fa39acb187225284c49a40cf5
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-04-04 17:49:06 -0400

    Add files via upload

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,17 +1,11 @@
-if typeof(syn) ~= "table" and Krnl.Base64.Decode then
-    syn.crypt.base64.decode = Krnl.Base64.Decode;
-elseif typeof(syn) ~= "table" and crypt.base64encode then
-    syn.crypt.base64.decode = crypt.base64decode;
-end
-
 if isfile("AlphaZero/Loader.lua") then
-    loadfile(syn.crypt.base64.decode("AlphaZero/Loader.lua"))();
+    loadfile("AlphaZero/Loader.lua")()
 else
     local Status, Script = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Uvxtq/AlphaZero/main/Loader.lua");
 
     if Status then
         loadstring(Script)()
     else
-        game:GetService("Players").LocalPlayer:Kick("Failed to grab loader, join the discord for support.");
+        cloneref(game:GetService("Players")).LocalPlayer:Kick("Failed to grab loader, join the discord for support.");
     end
 end
